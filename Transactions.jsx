d2e6039a430521f5636880a177c8f85c2fcb48c5
Theerchana Subramani <theershanas@gmail.com>
2025-10-08 10:30:03 +0530

    Add files via upload

---
+++ Transactions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Transactions() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-2xl font-bold text-indigo-600">Transactions Page</h1>
+      <p className="text-gray-600 mt-2">
+        Here you can view all your textbook exchange transactions.
+      </p>
+    </div>
+  );
+}
